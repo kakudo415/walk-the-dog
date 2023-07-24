1d8c824af178680190995f3dbbc1b7c423842026
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-07-24 14:03:44 +0900

    feat: Render Sierpinski triangle

--- walk-the-dog/src/lib.rs
+++ walk-the-dog/src/lib.rs
@@ -1,5 +1,51 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use rand::prelude::*;
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], color: (u8, u8, u8)) {
+    let [top, left, right] = points;
+
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+
+    context.stroke();
+    context.fill();
+}
+
+fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], color: (u8, u8, u8), depth: u32) {
+    draw_triangle(&context, points, color);
+
+    let depth = depth - 1;
+    if depth > 0 {
+        let [top, left, right] = points;
+
+        let left_mid = midpoint(top, left);
+        let right_mid = midpoint(top, right);
+        let bottom_mid = midpoint(left, right);
+
+        let mut rng = thread_rng();
+        let next_color = (
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+        );
+
+        sierpinski(context, [top, left_mid, right_mid], next_color, depth);
+        sierpinski(context, [left_mid, left, bottom_mid], next_color, depth);
+        sierpinski(context, [right_mid, bottom_mid, right], next_color, depth);
+    }
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -21,14 +67,7 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.move_to(300.0, 0.0); // top of triangle
-    context.begin_path();
-    context.line_to(0.0, 600.0); // bottom left of triangle
-    context.line_to(600.0, 600.0); // bottom right of triangle
-    context.line_to(300.0, 0.0); // back to top of triangle
-    context.close_path();
-    context.stroke();
-    context.fill();
+    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], (100, 200, 0), 5);
 
     Ok(())
 }
